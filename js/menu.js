d2b3d2b91cef76e3c8250b99fea9e970369fac11
Alejandro Arteaga <alejoarteaga2002@gmail.com>
2024-07-01 16:57:08 -0500

    Adding Responsive

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const aside = document.querySelector("aside");
+
+openMenu.addEventListener("click", () => {
+    aside.classList.add("visible-aside");
+})
+
+closeMenu.addEventListener("click", () => {
+    aside.classList.remove("visible-aside");
+})
+
+buttonsCategories.forEach(buttonn => buttonn.addEventListener("click", () => {
+    aside.classList.remove("visible-aside")
+}))
